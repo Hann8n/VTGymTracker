e401730286a8686080bcd444db817dc2a30c670e
Jack Hannon <hannon@vt.edu>
2024-09-26 19:27:39 -0400

    Initial Commit

---
+++ Gym Tracker (RC)/Gym_Tracker__RC_App.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_Tracker__RC_App.swift
+//  Gym Tracker (RC)
+//
+//  Created by Jack on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_Tracker__RC_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
